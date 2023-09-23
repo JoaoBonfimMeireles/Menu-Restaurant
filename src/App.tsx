345d53fd97fcada4e9ebf99c109003c3a016912c
JOAO MEIRELES <128010364+JoaoBonfimMeireles@users.noreply.github.com>
2023-09-23 01:49:19 -0300

    add img

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,29 @@ import React, { useState } from "react";
 import "./App.css";
 import { AllFoods } from "./Data/Data";
 import Card from "./componentes/Card/Card";
-import Buttons from "./componentes/Buttons/Buttons";
 
 function App() {
   const [food, setFood] = useState(AllFoods);
-  const menuItems = AllFoods.map((test) => test.category);
+  const categories = Array.from(new Set(AllFoods.map((item) => item.category)));
+
+  const filterFoodByCategory = (category: string) => {
+    const filteredFood = AllFoods.filter((item) => item.category === category);
+    setFood(filteredFood);
+  };
 
   return (
     <div className="App">
+      {categories.map((category) => (
+        <button key={category} onClick={() => filterFoodByCategory(category)}>
+          {category}
+        </button>
+      ))}
+      <button onClick={() => setFood(AllFoods)}>
+        Todos os pratos
+      </button>
       <div>
-        <h1>Aplicatido - Filtro de comida</h1>
-        <Buttons menuItems={menuItems}/>
-        <Card food={food}/>
+        <h1>Aplicativo - Filtro de Comida</h1>
+        <Card food={food} />
       </div>
     </div>
   );
